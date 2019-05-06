1b0d5966e2e376cbeac81b69833b9550d14fac64
Shawn <abc@gmail.com>
2019-05-06 15:43:03 +0800

    Fuck

--- Try4/Program.cs
+++ Try4/Program.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace Try4 {
     class Program {
         static void Main(string[] args) {
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            int number;
+            while (!Int32.TryParse(input, out number)) {
+                Console.WriteLine("Error: Please enter a number: ");
+            }
+
+            for (int i = 1; i<number+1; i++) {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
+
     }
 }
